eec26aeb5f62fec08daf6d246ef6359f326deaeb
arsalanmak <ashu.maknojia18@gmail.com>
2024-04-07 14:24:58 +0500

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Navbar from "./components/navbar.js";
+import { Banner } from "./components/Banner.js";
+import { Skills } from "./components/Skills.js";
+import { Projects } from "./components/Project.js";
+// import { Contact } from "./components/Contact.js";
+import { Footer } from "./components/Footer.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Banner />
+      <Skills />
+      <Projects />
+      {/* <Contact /> */}
+      <Footer />
     </div>
   );
 }
